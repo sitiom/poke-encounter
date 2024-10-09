addc4ba06583fab18bd3770a4beee422d048a5a7
Ryan Caezar Itang <ryancaezar.itang@trajectorservices.com>
2024-10-10 05:16:03 +0800

    feat: add query example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import { useState } from "react";
 import { Button, TextInput } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { NamedAPIResource } from "pokeapi-js-wrapper";
 
 function App() {
+  const [limit, setLimit] = useState(3);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["random", limit],
+    queryFn: async () => {
+      const { data } = await axios.get<NamedAPIResource | NamedAPIResource[]>(
+        "http://localhost:3000/pokemon/random",
+        {
+          params: {
+            limit,
+          },
+        }
+      );
+      console.log(data);
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error!</div>;
+  }
+
+  if (data instanceof Array) {
+    return (
+      <ul>
+        {data.map((pokemon) => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+      </ul>
+    );
+  }
   return (
     <>
       <Button
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider defaultColorScheme="dark">
-      <App />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider defaultColorScheme="dark">
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </MantineProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
