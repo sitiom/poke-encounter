20ac399583106fce02f6e2fccbd0fc4258c3cdde
Ryan Caezar Itang <ryancaezar.itang@trajectorservices.com>
2024-10-11 03:05:07 +0800

    feat: add auto animate libary

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,27 +4,31 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import logo from "../assets/logo.png";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <MantineProvider defaultColorScheme="dark">
-          <header className="flex my-8 flex-col items-center">
-            <img src={logo} alt="logo" className="w-52" />
-            <div className="font-semibold text-xl">
-              React + Express Battle Simulator
+  component: () => {
+    const [parent] = useAutoAnimate();
+    return (
+      <>
+        <QueryClientProvider client={queryClient}>
+          <MantineProvider defaultColorScheme="dark">
+            <header className="flex my-8 flex-col items-center">
+              <img src={logo} alt="logo" className="w-52" />
+              <div className="font-semibold text-xl">
+                React + Express Battle Simulator
+              </div>
+            </header>
+            <div className="container w-full mx-auto px-12" ref={parent}>
+              <Outlet />
             </div>
-          </header>
-          <div className="container w-full mx-auto px-12">
-            <Outlet />
-          </div>
-          <TanStackRouterDevtools />
-          <ReactQueryDevtools />
-        </MantineProvider>
-      </QueryClientProvider>
-    </>
-  ),
+            <TanStackRouterDevtools />
+            <ReactQueryDevtools />
+          </MantineProvider>
+        </QueryClientProvider>
+      </>
+    );
+  },
 });
